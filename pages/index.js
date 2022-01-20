06e763d472db18923a6d214e6714be78f2139057
Amaro <amaro@solutiontrue.com.br>
2022-01-20 14:36:48 -0300

    remove mudar

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,6 @@ import { Card, Text } from './components/Card'
 export default function Home() {
   const [arrActive, setArrActive] = useState([false, false, true, false]);
 
-  function mudar() {
-    setInterval(() => {
-      const primeitoItem = arrActive[0];
-      const novoArr = arrActive;
-      novoArr.shift();
-      novoArr.push(primeitoItem);
-      setArrActive(novoArr);
-      console.log('🟢', arrActive);
-    }, 5000);
-  }
-
   useEffect(() => {
     setInterval(() => {
       const primeitoItem = arrActive[0];
